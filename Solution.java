1606e50c550c1c89e2c248fbb0b5ef366fce5d1a
sdim-ua <ingersol@yandex.ua>
2019-06-18 00:23:48 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    
+    //Напишите тут ваши методы
+     public static void print (int x){
+        System.out.println(x);
+    }
+    
+     public static void print (int x, int a){
+        System.out.println(x);
+    }
+    
+     public static void print (int x, int a, int b){
+        System.out.println(x);
+    }
+    
+     public static void print (int x, int a, int b, int y){
+        System.out.println(x);
+    }
+    
+     public static void print (int x, int a, int b, int y, int z){
+        System.out.println(x);
+    }
+}
